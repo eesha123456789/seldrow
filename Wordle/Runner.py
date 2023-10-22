d77546869c84c525ad644016e4b8c557ef0b4b3e
Eesha <73623905+eesha123456789@users.noreply.github.com>
2023-10-22 21:09:24 +0000

    added Wordle class and two functions

--- Wordle/Runner.py
+++ Wordle/Runner.py
@@ -56,3 +56,18 @@ current_guess_string = ""
 current_letter_bg_x = 110
 
 game_result = ""
+
+lass Wordle:
+    def __init__(self, x, y, letter):
+        self.x = x
+        self.y = y
+        self.text = letter
+        self.rect = (x, y, 57, 75)
+        self.bg_color = OUTLINE
+
+    def draw(self):
+        pygame.draw.rect(SCREEN, self.bg_color, self.rect)
+        self.text_surface = AVAILABLE_LETTER_FONT.render(self.text, True, "blue")
+        self.text_rect = self.text_surface.get_rect(center=(self.x+27, self.y+30))
+        SCREEN.blit(self.text_surface, self.text_rect)
+        pygame.display.update()
